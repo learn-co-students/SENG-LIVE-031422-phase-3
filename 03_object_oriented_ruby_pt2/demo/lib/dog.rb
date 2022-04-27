48018bc761f4f3628d5539cb97e577d16ca822b6
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-04-26 22:15:17 -0700

    update starter code for lecture 3

--- 03_object_oriented_ruby_pt2/demo/lib/dog.rb
+++ 03_object_oriented_ruby_pt2/demo/lib/dog.rb
@@ -1,15 +1,6 @@
-class Dog 
+class Dog
 
   attr_accessor :name, :age, :breed, :favorite_treats, :last_walked_at, :last_fed_at
-  # will generate these methods below
-  # def name
-  #   @name 
-  # end
-
-  # def name=(name)
-  #   @name = name
-  # end
-
   def initialize(name, age, breed, favorite_treats)
     @name = name
     @age = age
@@ -24,17 +15,90 @@ class Dog
   def feed
     @last_fed_at = Time.now
   end
-  
-  def print 
+
+  def print
     puts ""
-    puts @name.light_green
-    puts "  age: #{@age}"
-    puts "  breed: #{@breed}"
-    puts "  favorite_treats: #{@favorite_treats}"
-    puts "  last walked at: #{@last_walked_at}"
-    puts "  last fed at: #{@last_fed_at}"
+    puts @name.green
+    puts "  Age: #{@age}"
+    puts "  Breed: #{@breed}"
+    puts "  Favorite Treats: #{@favorite_treats}"
+    if needs_a_walk?
+      puts "  Last Walked At: #{@last_walked_at}".red
+    else
+      puts "  Last Walked At: #{@last_walked_at}"
+    end
+    if needs_a_meal?
+      puts "  Last Fed At: #{@last_fed_at}".red
+    else
+      puts "  Last Fed At: #{@last_fed_at}"
+    end
     puts ""
   end
 
+  def needs_a_meal?
+    if !@last_fed_at
+      true
+    else 
+      @last_fed_at < 15.seconds.ago
+    end
+  end
 
-end
+  def needs_a_walk?
+    if !@last_walked_at
+      true
+    else
+      @last_walked_at < 10.seconds.ago
+    end
+  end
+
+  # given by attr_accessor
+  # def name
+  #   @name
+  # end
+
+  # def name=(name)
+  #   @name = name
+  # end
+
+
+  # def age
+  #   @age
+  # end
+
+  # def age=(age)
+  #   @age = age
+  # end
+
+  # def breed
+  #   @breed
+  # end
+
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+
+  # def favorite_treats
+  #   @favorite_treats
+  # end
+
+  # def favorite_treats=(favorite_treats)
+  #   @favorite_treats = favorite_treats
+  # end
+
+  # def last_walked_at
+  #   @last_walked_at
+  # end
+
+  # def last_walked_at=(last_walked_at)
+  #   @last_walked_at = last_walked_at
+  # end
+
+  # def last_fed_at
+  #   @last_fed_at
+  # end
+
+  # def last_fed_at=(last_fed_at)
+  #   @last_fed_at = last_fed_at
+  # end
+
+end 
--- 03_object_oriented_ruby_pt2/demo/lib/dog_walker_cli.rb
+++ 03_object_oriented_ruby_pt2/demo/lib/dog_walker_cli.rb
@@ -1,103 +1,113 @@
-DOGS = [Dog.new("Lennon", "1 year", "Pomeranian", "cheese")]
+DOGS = [
+  Dog.new("Lennon Snow", "almost 2", "Pomeranian", "Cheese")
+]
 
 def start_cli
-  puts "hello! Welcome to the Dog Walker CLI"
+  puts "Hi there! Welcome to the Dog Walker CLI".cyan
   print_menu_options
-  choice = ask_for_choice
-  until choice == "exit"
-    handle_user_choice(choice)
-    choice = ask_for_choice
+  loop do
+    handle_choice(ask_for_choice)
+    print_menu_options
   end
-  puts "Thank you for using the Dog Walker CLI!"
 end
 
 def print_menu_options
-  puts "Here's a list of the options. Type:".cyan
-  puts "  1. To add a dog".cyan
-  puts "  2. To list dogs".cyan
-  puts "  3. To feed a dog".cyan
-  puts "  4. To walk a dog".cyan
-  puts "  menu to show the options again, or".cyan
-  puts "  exit to leave the program".cyan
+  puts "What would you like to do?".cyan
+  puts "  1. Add a Dog"
+  puts "  2. List Dogs"
+  puts "  3. To walk a Dog"
+  puts "  4. To feed a Dog"
+  puts "type 'menu' to see these choices again"
+  puts "or type exit at any point to leave the program"
 end
 
 def ask_for_choice
-  print "What would you like to do? "
-  gets.chomp
+  choice = gets.chomp
+  if choice == "exit"
+    puts "Thanks for using the Dog Walker CLI! Have a nice day :)"
+    exit 
+  else
+    choice
+  end
 end
 
-def handle_user_choice(choice)
+def handle_choice(choice)
   if choice == "1"
     add_dog
   elsif choice == "2"
     list_dogs
   elsif choice == "3"
-    feed_dog
-  elsif choice == "4"
     walk_dog
+  elsif choice == "4"
+    feed_dog
+  elsif choice == "debug"
+    binding.pry
   elsif choice == "menu"
     print_menu_options
-  elsif choice == "debug" 
-    binding.pry
-  else 
-    puts "Whoops! I didn't understand your choice".red
+  else
+    "Whoops! I didn't understand that choice, please try again.".red
   end
-  print_menu_options
 end
 
 def add_dog
-  puts "Sure! Let's add your dog!"
   print "What's your dog's name? "
-  name = gets.chomp
+  name = ask_for_choice
   print "What's your dog's age? "
-  age = gets.chomp
+  age = ask_for_choice
   print "What's your dog's breed? "
-  breed = gets.chomp
-  print "What are your dog's favorite treats? "
-  favorite_treats = gets.chomp
-  # make a new instance of dog with the user's answers
+  breed = ask_for_choice
+  print "What's your dog's favorite treats? "
+  favorite_treats = ask_for_choice
+  # dog_hash = {
+  #   name: dog_name,
+  #   age: dog_age,
+  #   breed: dog_breed,
+  #   favorite_treats: dog_favorite_treats
+  # }
+  # DOGS << dog_hash
+  # print_dog(dog_hash)
   dog = Dog.new(name, age, breed, favorite_treats)
-  DOGS.push(dog)
+  DOGS << dog
   dog.print
 end
 
 def list_dogs
-  puts "here are the dogs"
   DOGS.each do |dog|
     dog.print
   end
 end
 
-def feed_dog
-  # List dogs with a number up front and ask a user to choose the number corresponding
-  # to the dog they want to feed
-  puts "Which dog would you like to feed? (type in their number)"
-  DOGS.each.with_index(1) do |dog, index|
-    puts "#{index}. #{dog.name} (#{dog.breed})"
+def walk_dog
+  puts "Choose a dog to walk by its corresponding number"
+  DOGS.each.with_index(1) do |dog, num|
+    puts "#{num}. #{dog.name}"
   end
-  dog_index = gets.chomp.to_i - 1
-  until dog_index >= 0
-    puts "Whoops! That didn't work".red
-    puts "Please type the number corresponding to the dog you'd like to feed"
-    dog_index = gets.chomp.to_i - 1
+  index = ask_for_choice.to_i - 1
+  dog = DOGS[index]
+  until dog
+    puts "Whoops! Please choose a number from the choices above".red
+    index = ask_for_choice.to_i - 1
+    dog = DOGS[index]
   end
-  dog = DOGS[dog_index]
-  dog.feed
+  dog.walk
   dog.print
 end
 
-def walk_dog
-  puts "Which dog would you like to walk? (type in their number)"
-  DOGS.each.with_index(1) do |dog, index|
-    puts "#{index}. #{dog.name} (#{dog.breed})"
+def feed_dog
+  puts "Choose a dog to feed by its corresponding number"
+  DOGS.each.with_index(1) do |dog, num|
+    puts "#{num}. #{dog.name}"
   end
-  dog_index = gets.chomp.to_i - 1
-  until dog_index >= 0
-    puts "Whoops! That didn't work".red
-    puts "Please type the number corresponding to the dog you'd like to feed"
-    dog_index = gets.chomp.to_i - 1
+  index = ask_for_choice.to_i - 1
+  dog = DOGS[index]
+  # run a loop until we get a dog from their input
+  until dog
+    # while we don't have a dog give them an error
+    # and ask again
+    puts "Whoops! Please choose a number from the choices above".red
+    index = ask_for_choice.to_i - 1
+    dog = DOGS[index]
   end
-  dog = DOGS[dog_index]
-  dog.walk
+  dog.feed
   dog.print
 end
