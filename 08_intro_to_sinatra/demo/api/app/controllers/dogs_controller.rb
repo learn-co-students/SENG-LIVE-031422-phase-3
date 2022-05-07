05957694b0d74bd412a0ac5f860a9fe5ecefc16d
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-05-06 20:19:40 -0700

    end of lesson 8

--- 08_intro_to_sinatra/demo/api/app/controllers/dogs_controller.rb
+++ 08_intro_to_sinatra/demo/api/app/controllers/dogs_controller.rb
@@ -1,6 +1,25 @@
 class DogsController < ApplicationController
   
+  get "/dogs" do
+    # get all dogs from database:
+    Dog.all.to_json(methods: [:age])
+  end
+
+  post "/dogs" do 
+    Dog.create(dog_params).to_json(methods: [:age])
+  end
+
+  patch "/dogs/:id" do 
+    dog = Dog.find(params[:id])
+    dog.update(dog_params)
+    dog.to_json(methods: [:age])
+  end
   
+  delete "/dogs/:id" do 
+    dog = Dog.find(params[:id])
+    dog.destroy
+    status 204
+  end
 
   private 
 
@@ -8,7 +27,10 @@ class DogsController < ApplicationController
   # we use this method to create a list of what's permitted to be passed to .create or .update
   # within controller actions.
   def dog_params
-    
+    allowed_params = ["name", "birthdate", "breed", "image_url"]
+    params.filter do |param, value|
+      allowed_params.include?(param)
+    end
   end
 
 end
